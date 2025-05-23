9344013630a1b0c2080b62f1ca294906e5e2b546
kwame-Owusu <alfredowusu595@gmail.com>
2025-05-23 13:22:51 +0100

    now able to draw to screen with cells

--- graphics.py
+++ graphics.py
@@ -7,7 +7,7 @@ class Window:
     self.width = width
     self.height = height
     self.__root = Tk()
-    self.__root.title = "Maze Solver"
+    self.__root.title("Maze Solver") 
     self.canvas = Canvas(self.__root, bg="white", width=width, height=height)
     self.canvas.pack(fill=BOTH, expand=1)
     self.is_window_running = False
@@ -31,7 +31,7 @@ class Window:
     line.draw(self.canvas, fill_color)
 
 class Cell:
-  def __init__(self, window: Window) -> None:
+  def __init__(self, window: Window) -> None:  
     self.has_left_wall = True
     self.has_right_wall = True
     self.has_top_wall = True
@@ -41,25 +41,22 @@ class Cell:
     self.__y1 = -1
     self.__y2 = -1
     self.__win = window
-  
+ 
   def draw(self, x1, x2, y1, y2) -> None:
-    self.__x1 = x1 
-    self.__x2 = x2 
-    self.__y1 = y1 
-    self.__y2 = y2 
-
+    self.__x1 = x1
+    self.__x2 = x2
+    self.__y1 = y1
+    self.__y2 = y2
+    
     if self.has_left_wall:
-      line = Line(Point(x1, y1), Point(x1,y2))
+      line = Line(Point(x1, y1), Point(x1, y2))
       self.__win.draw_line(line)
-
     if self.has_right_wall:
-      line = Line(Point(x2, y1), Point(x2,y2))
+      line = Line(Point(x2, y1), Point(x2, y2))
       self.__win.draw_line(line)
-
     if self.has_top_wall:
-      line = Line(Point(x1, y1), Point(x2,y1))
+      line = Line(Point(x1, y1), Point(x2, y1))
       self.__win.draw_line(line)
-
     if self.has_bottom_wall:
-      line = Line(Point(x1, y2), Point(x2,y2))
+      line = Line(Point(x1, y2), Point(x2, y2))
       self.__win.draw_line(line)
--- main.py
+++ main.py
@@ -5,21 +5,21 @@ def main() -> None:
   win = Window(800, 600) 
   c = Cell(win)
   c.has_left_wall = False
-  c.draw(50, 50, 100, 100)
-
+  c.draw(50, 100, 50, 100)  # x1=50, x2=100, y1=50, y2=100
+  
   c = Cell(win)
   c.has_right_wall = False
-  c.draw(125, 125, 200, 200)
-
+  c.draw(125, 200, 125, 200)  # x1=125, x2=200, y1=125, y2=200
+  
   c = Cell(win)
   c.has_bottom_wall = False
-  c.draw(225, 225, 250, 250)
-
+  c.draw(225, 250, 225, 250)  # x1=225, x2=250, y1=225, y2=250
+  
   c = Cell(win)
   c.has_top_wall = False
-  c.draw(300, 300, 500, 500)
-
-  win.wait_for_close()
+  c.draw(300, 500, 300, 500)  # x1=300, x2=500, y1=300, y2=500
+    
+  win.wait_for_close() 
 
 if __name__ == "__main__":
   main()
