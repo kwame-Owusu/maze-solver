b749936d69420919bd4363ce7f3cb076501c1549
kwame-Owusu <alfredowusu595@gmail.com>
2025-05-23 12:07:07 +0100

    able to now draw window to screen and exit through x button

--- main.py
+++ main.py
@@ -5,17 +5,29 @@ class Window:
   def __init__(self, width: int, height: int) -> None:
     self.width = width
     self.height = height
-    self.root = Tk()
-    self.root.title = "Maze Solver"
+    self.__root = Tk()
+    self.__root.title = "Maze Solver"
     self.canvas = Canvas()
     self.canvas.pack()
     self.is_window_running = False
   
   def redraw(self) -> None:
-    pass
-  
-  def wait_for_close(self) -> None:
-    pass
+    """redraws all the graphics in the window"""
+    self.__root.update() 
+    self.__root.update_idletasks()
 
+  def wait_for_close(self) -> None:
+    self.is_window_running = True
+    while self.is_window_running:
+      self.redraw()
   def close(self) -> None:
-    pass
+    self.is_window_running = False
+    self.__root.protocol("WM_DELETE_WINDOW", self.close)
+  
+
+def main() -> None:
+  win = Window(800, 900)
+  win.wait_for_close()
+
+if __name__ == "__main__":
+  main()
