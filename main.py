5c0a231621458662fbec1bcdcf048c831cd9f348
kwame-Owusu <alfredowusu595@gmail.com>
2025-05-23 11:58:35 +0100

    started class constructor and will implement function definitions

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+  def __init__(self, width: int, height: int) -> None:
+    self.width = width
+    self.height = height
+    self.root = Tk()
+    self.root.title = "Maze Solver"
+    self.canvas = Canvas()
+    self.canvas.pack()
+    self.is_window_running = False
+  
+  def redraw(self) -> None:
+    pass
+  
+  def wait_for_close(self) -> None:
+    pass
+
+  def close(self) -> None:
+    pass
