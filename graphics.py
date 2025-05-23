3c334663797bd2276982e27cbb30f8fced153c0f
kwame-Owusu <alfredowusu595@gmail.com>
2025-05-23 13:10:24 +0100

    added cells but unable to draw to screen, will fix

--- graphics.py
+++ graphics.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
-from point import Line
+from point import Line, Point
 
 
 class Window:
@@ -29,3 +29,37 @@ class Window:
   
   def draw_line(self, line: Line, fill_color: str = "black") -> None:
     line.draw(self.canvas, fill_color)
+
+class Cell:
+  def __init__(self, window: Window) -> None:
+    self.has_left_wall = True
+    self.has_right_wall = True
+    self.has_top_wall = True
+    self.has_bottom_wall = True
+    self.__x1 = -1
+    self.__x2 = -1
+    self.__y1 = -1
+    self.__y2 = -1
+    self.__win = window
+  
+  def draw(self, x1, x2, y1, y2) -> None:
+    self.__x1 = x1 
+    self.__x2 = x2 
+    self.__y1 = y1 
+    self.__y2 = y2 
+
+    if self.has_left_wall:
+      line = Line(Point(x1, y1), Point(x1,y2))
+      self.__win.draw_line(line)
+
+    if self.has_right_wall:
+      line = Line(Point(x2, y1), Point(x2,y2))
+      self.__win.draw_line(line)
+
+    if self.has_top_wall:
+      line = Line(Point(x1, y1), Point(x2,y1))
+      self.__win.draw_line(line)
+
+    if self.has_bottom_wall:
+      line = Line(Point(x1, y2), Point(x2,y2))
+      self.__win.draw_line(line)
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from graphics import Window 
+from graphics import Window, Cell
 from point import Line, Point
 
 def main() -> None:
-  win = Window(800, 900)
-  point1= Point(50,50) 
-  point2= Point(400,400) 
-  line = Line(point1, point2)
-  win.draw_line(line, "black")
+  win = Window(800, 600) 
+  c = Cell(win)
+  c.has_left_wall = False
+  c.draw(50, 50, 100, 100)
+
+  c = Cell(win)
+  c.has_right_wall = False
+  c.draw(125, 125, 200, 200)
+
+  c = Cell(win)
+  c.has_bottom_wall = False
+  c.draw(225, 225, 250, 250)
+
+  c = Cell(win)
+  c.has_top_wall = False
+  c.draw(300, 300, 500, 500)
+
   win.wait_for_close()
 
 if __name__ == "__main__":
