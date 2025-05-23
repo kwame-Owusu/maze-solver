32518e55522871da7aba88e67b9d9fcae42188a7
kwame-Owusu <alfredowusu595@gmail.com>
2025-05-23 12:15:13 +0100

    moved class to a different file and added git ignore for pycache

---
+++ graphics.py
@@ -0,0 +1,28 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+  def __init__(self, width: int, height: int) -> None:
+    self.width = width
+    self.height = height
+    self.__root = Tk()
+    self.__root.title = "Maze Solver"
+    self.canvas = Canvas(self.__root, bg="white", width=width, height=height)
+    self.canvas.pack(fill=BOTH, expand=1)
+    self.is_window_running = False
+    self.__root.protocol("WM_DELETE_WINDOW", self.close)
+  
+  def redraw(self) -> None:
+    """redraws all the graphics in the window"""
+    self.__root.update_idletasks()
+    self.__root.update() 
+
+  def wait_for_close(self) -> None:
+    self.is_window_running = True
+    while self.is_window_running:
+      self.redraw()
+    print("window closed..")
+
+  def close(self) -> None:
+    self.is_window_running = False
+  
--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-from tkinter import Tk, BOTH, Canvas
-
-
-class Window:
-  def __init__(self, width: int, height: int) -> None:
-    self.width = width
-    self.height = height
-    self.__root = Tk()
-    self.__root.title = "Maze Solver"
-    self.canvas = Canvas()
-    self.canvas.pack()
-    self.is_window_running = False
-  
-  def redraw(self) -> None:
-    """redraws all the graphics in the window"""
-    self.__root.update() 
-    self.__root.update_idletasks()
-
-  def wait_for_close(self) -> None:
-    self.is_window_running = True
-    while self.is_window_running:
-      self.redraw()
-  def close(self) -> None:
-    self.is_window_running = False
-    self.__root.protocol("WM_DELETE_WINDOW", self.close)
-  
+from graphics import Window 
 
 def main() -> None:
   win = Window(800, 900)
